3a801c2ebecfe16344cccfb356d4b461a3378518
Celina Alzenor <calzenor@outlook.com>
2026-03-19 15:01:34 -0700

    create upload to gcs function

---
+++ event-wall-api/storage/gcs.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv, dotenv_values
+from google.cloud import storage
+
+load_dotenv()
+
+STORAGE_CLIENT = storage.Client()
+EVENTS_BUCKET = os.getenv("GCS_EVENT_BUCKET")
+ASSETS_BUCKET = os.getenv("GCS_ASSETS_BUCKET")
+
+def upload_to_gcs(data: bytes, slug: str, prefix:str, upload_id: str, to_events=True):
+   """Uploads a file to the bucket."""
+
+   dest_path = os.path.join(slug, prefix, upload_id)
+
+   bucket_name = EVENTS_BUCKET if to_events else ASSETS_BUCKET
+   db_url = dest_path if to_events else f"https://storage.googleapis.com/{ASSETS_BUCKET}/{dest_path}"
+
+   bucket = STORAGE_CLIENT.bucket(bucket_name)
+   blob = bucket.blob(dest_path)
+
+   blob.upload_from_string(data)
+
+   return db_url
+
+
+
